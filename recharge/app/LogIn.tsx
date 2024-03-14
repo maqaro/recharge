5ca5ecf3617a7dd39c116187f7ac41a5c75c4555
Your Name <you@example.com>
2024-03-14 11:49:15 +0000

    Changed LogIn

--- recharge/app/LogIn.tsx
+++ recharge/app/LogIn.tsx
@@ -4,63 +4,72 @@ import { Input, Button } from 'react-native-elements';
 import { color } from 'react-native-elements/dist/helpers';
 import { LinearGradient } from 'expo-linear-gradient';
 
+ 
+
 const Login = () => {
   return (
     <View style={styles.container}>
       <LinearGradient colors={['#1a7373', '#e37b60']} style={{height:'100%'}}>
-
         <View style={styles.textContainer}>
         <Text style={styles.welcomeText}>Welcome</Text>
         <Text style={styles.backText}>Back!</Text>
         </View>
       <View style={styles.formContainer}>
         <Input
-          placeholder="Username"
+          placeholder="   Username"
           leftIcon={{ type: 'font-awesome', name: 'user', color: 'white' }}
           inputContainerStyle={styles.username}
           placeholderTextColor='white'
         />
+
         <Input
-          placeholder="Password"
+          placeholder="   Password"
           secureTextEntry
           leftIcon={{ type: 'font-awesome', name: 'lock', color:'white' }}
           inputContainerStyle={styles.inputContainer}
           placeholderTextColor='white'
         />
+
         <Text style={styles.forgot}>Forgot Password?</Text>
-        <Button 
-          title="Login" 
-          buttonStyle={styles.button} 
+        <Button
+          title="Login"
+          buttonStyle={styles.LoginButton}
           titleStyle={{color: '#b7410e'}}
-          />
+        />
+
         <Text style={styles.or}> ------------------------------- OR ------------------------------</Text>
-        <Button 
-          title="Sign up" 
-          buttonStyle={styles.button}
+
+        <Button
+          title="Sign up"
+          buttonStyle={styles.SignupButton}
           titleStyle={{color: '#b7410e'}}
-           />
+         />
 
-
-           
       </View>
     </LinearGradient>
     </View>
   );
 };
 
+ 
+
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
   },
+
   logoContainer: {
     alignItems: 'center',
     marginTop: 50,
   },
+
   logo: {
     width: 200,
     height: 200,
     resizeMode: 'contain',
   },
+
   textContainer: {
     flexDirection: 'column',
     justifyContent: 'flex-start',
@@ -68,46 +77,75 @@ const styles = StyleSheet.create({
     marginLeft: 10,
     marginBottom: 80,
   },
+
   welcomeText: {
     fontSize: 50,
     textAlign: 'center',
     color: 'white',
-    marginTop: 30,
+    marginTop: 40,
   },
+
   backText: {
     fontSize: 50,
     textAlign: 'center',
     color: 'white',
   },
+
   formContainer: {
     marginTop: 35,
     color:'white',
   },
+
   username:{
     borderBottomColor: 'white',
     marginBottom: 40,
   },
 
+ 
+
   inputContainer: {
     borderBottomColor: 'white',
   },
 
+ 
+
   forgot: {
     textAlign: 'right',
-    color:'#b7410e',
-    marginTop: 20,
+    marginRight: 10,
+    color:'#195353',
+    fontWeight: 'bold',
+    marginTop: 10,
+    fontSize: 15,
   },
+
   or: {
     color:'white',
     marginTop: 10,
+    textAlign: 'center',
   },
-  button: {
-    width: '100%',
-    marginTop: 20,
+
+  LoginButton: {
+    width: '30%',
+    marginTop: 80,
     borderRadius: 20,
     backgroundColor: 'white',
+    height: 50,
+    marginLeft: 143,
   },
+
+ 
+
+  SignupButton: {
+    width: '30%',
+    marginTop: 10,
+    borderRadius: 20,
+    backgroundColor: 'white',
+    height: 50,
+    marginLeft: 143,
+  },
+
 });
 
-export default Login;
+ 
 
+export default Login;

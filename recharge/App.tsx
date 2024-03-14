e9707691bfd3149b15214833b6e276e97f8805f1
DavinaNaran22 <157966175+DavinaNaran22@users.noreply.github.com>
2024-03-14 12:20:56 +0000

    Changes

--- recharge/App.tsx
+++ recharge/App.tsx
@@ -1,17 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native';
-<<<<<<< HEAD
-import EmailForm from './components/EmailForm';
-import Opening from './components/Opening';
-import Login from './components/LogIn';
-import SignUp from './components/SignUp';
-import HomePage from './components/Homepage';
-import StackNavigator from './components/StackNavigator';
+import Opening from './app/Opening';
+import Login from './app/LogIn';
+import SignUp from './app/SignUp';
+import HomePage from './app/Homepage';
+import StackNavigator from './app/StackNavigator';
 import { NavigationContainer } from '@react-navigation/native';
-import Trackers from './components/Trackers';
+import Trackers from './app/Trackers';
 
-=======
-import EmailForm from './app';
->>>>>>> 02362c107041c389fabea28ec5d8824791aa3d57
 
 export default function App() {
   return (
--- recharge/app/Homepage.tsx
+++ recharge/app/Homepage.tsx
@@ -3,48 +3,51 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { useRouter } from 'expo-router';
+import { LinearGradient } from 'expo-linear-gradient';
 
 
 export default function Homepage() {
     const router = useRouter();
     return (
         <View style={styles.container}>
+          <LinearGradient colors={['#1a7373', '#e37b60']} style={{height:'100%', width:'100%'}}>
           <TouchableOpacity
-            style={styles.square}
+            style={styles.square1}
             onPress={() => router.navigate('/Trackers')}
           >
             <Text style={styles.text}>Trackers</Text>
           </TouchableOpacity>
           <TouchableOpacity
-            style={styles.square}
+            style={styles.square2}
             //onPress={() => ........
           >
             <Text style={styles.text}>Daily Journal</Text>
           </TouchableOpacity>
           <TouchableOpacity
-            style={styles.square}
+            style={styles.square3}
             //onPress={() => .........
           >
             <Text style={styles.text}>Mentor Match</Text>
           </TouchableOpacity>
           <TouchableOpacity
-            style={styles.square}
+            style={styles.square4}
             //onPress={() => ........
           >
             <Text style={styles.text}>Mental Health Resources</Text>
           </TouchableOpacity>
           <TouchableOpacity
-            style={styles.square}
+            style={styles.square5}
             //onPress={() => h........
           >
             <Text style={styles.text}>Exercise Routines</Text>
           </TouchableOpacity>
           <TouchableOpacity
-            style={styles.square}
+            style={styles.square6}
             //onPress={() => ..........
           >
             <Text style={styles.text}>Additonal Resources</Text>
           </TouchableOpacity>
+        </LinearGradient>
         </View>
       );
 };
@@ -58,7 +61,7 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       backgroundColor: '#fff',
     },
-    square: {
+    square1: {
       width: '45%',
       aspectRatio: 1, // Square aspect ratio
       margin: '2.5%', // Adjust as needed
@@ -66,6 +69,52 @@ const styles = StyleSheet.create({
       justifyContent: 'center',
       alignItems: 'center',
     },
+    
+    square2: {
+      width: '45%',
+      aspectRatio: 1, // Square aspect ratio
+      margin: '2.5%', // Adjust as needed
+      backgroundColor: 'lightblue',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+
+    square3: {
+      width: '45%',
+      aspectRatio: 1, // Square aspect ratio
+      margin: '2.5%', // Adjust as needed
+      backgroundColor: 'lightblue',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+
+    square4: {
+      width: '45%',
+      aspectRatio: 1, // Square aspect ratio
+      margin: '2.5%', // Adjust as needed
+      backgroundColor: 'lightblue',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+
+    square5: {
+      width: '45%',
+      aspectRatio: 1, // Square aspect ratio
+      margin: '2.5%', // Adjust as needed
+      backgroundColor: 'lightblue',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+
+    square6: {
+      width: '45%',
+      aspectRatio: 1, // Square aspect ratio
+      margin: '2.5%', // Adjust as needed
+      backgroundColor: 'lightblue',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+
     text: {
       fontSize: 18,
       fontWeight: 'bold',

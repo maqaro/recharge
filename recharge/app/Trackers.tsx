31b93465246c3f0bf969f37e8777ff7da6062bb2
Jeenat-03 <jeenathussain1@gmail.com>
2024-03-21 12:36:52 +0000

    Tracker changed

--- recharge/app/Trackers.tsx
+++ recharge/app/Trackers.tsx
@@ -19,7 +19,7 @@ const Trackers = () => {
         {/* <ScrollView contentContainerStyle={styles.scrollContainer}> */}
 
           <TouchableOpacity style={[styles.Step1]} onPress={() => console.log('Rectangle 1 clicked')}>
-          <ImageBackground imageStyle={{borderRadius: 20}} source={require('./images/step_bg.jpg')}>
+          <ImageBackground imageStyle={{borderRadius: 20}} source={require('./images/StepCounter.png')}>
             <Text style={styles.text}>Step Tracker</Text>
             <Image source={require('./images/Step.png')} style={styles.StepPic}/>
           </ImageBackground>

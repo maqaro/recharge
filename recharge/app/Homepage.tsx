530c6ec1bbc5444c621eb10c970729fe38ca9818
Jeenat Hussain <59393908+Jeenat-03@users.noreply.github.com>
2024-04-04 21:09:09 +0100

    Update Homepage.tsx

--- recharge/app/Homepage.tsx
+++ recharge/app/Homepage.tsx
@@ -5,40 +5,11 @@ import { View, Text, TouchableOpacity, StyleSheet, Image, Button, ImageBackgroun
 import { useRouter } from 'expo-router';
 import { LinearGradient } from 'expo-linear-gradient';
 // import NavBar from './NavBar';
-// import notifee from "@notifee/react-native";
 import TrackerNav from './TrackerNav';
 import NavBar from './NavBar';
 
 export default function Homepage() {
     const router = useRouter();
-
-    // async function onDisplayNotification() {
-
-    //   // Request permissions (required for iOS)
-    //   await notifee.requestPermission();
-  
-    //   // Create a channel (required for Android)
-    //   const channelId = await notifee.createChannel({
-    //     id: "default",
-    //     name: "Default Channel",
-    //   });
-  
-    //   // Display a notification
-    //   await notifee.displayNotification({
-    //     title: "Water Reminder",
-    //     body: "Remeber to drink water and stay hydrated!",
-    //     android: {
-    //       channelId,
-    //       // pressAction is needed if you want the notification to open the app when pressed
-    //       pressAction: {
-    //         id: "default",
-    //       },
-    //     },
-    //   });
-    // }
-
-    
-
 
     return (
         <View style={styles.container}>

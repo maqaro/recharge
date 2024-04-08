4601d4c32680340d3a71c2126f622626cd58a003
dodsey123 <115038686+dodsey123@users.noreply.github.com>
2024-04-08 14:55:59 +0100

    Add files via upload

--- recharge/app/ViewFriends.tsx
+++ recharge/app/ViewFriends.tsx
@@ -1,9 +1,11 @@
 // Homepage.tsx
 
 import React, {useEffect, useState} from 'react';
-import { View, Text, StyleSheet, Button, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, Button, TouchableOpacity, ScrollView } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { supabase } from '../lib/supabase';
+import NavBar from './NavBar';
+import { useRouter } from 'expo-router';
 
 const ViewFriends = () => {
     const [userid, setUserid] = useState<string | undefined>();
@@ -11,6 +13,8 @@ const ViewFriends = () => {
     const [pending, setPending] = useState<any[]>([]);
     const [requests, setRequests] = useState<any[]>([]);
     const [name, setName] = useState<any[]>([]);
+
+    const router = useRouter();
 
     useEffect(() => {
         getFriends();
@@ -130,13 +134,13 @@ const ViewFriends = () => {
 
       const displayFriends = () =>{
         if (friends.length == 0){
-            return <Text style={styles.details}>You have no friends lol</Text>
+            return <Text style={styles.title}>You have no friends lol</Text>
         }
         else{
             return(
-                <View style={styles.item}>
+                <View>
                 {friends?.map(item => (
-                    <View>
+                    <View style={styles.inneritem}>
                         <Text style={styles.title}>{item}</Text>
                 </View>
                 ))}
@@ -147,13 +151,13 @@ const ViewFriends = () => {
 
       const displayPending = () =>{
         if (pending.length == 0){
-            return <Text style={styles.details}>You currently do not have any pending requests.</Text>
+            return <Text style={styles.title}>You currently do not have any pending requests.</Text>
         }
         else{
             return(
-                <View style={styles.item}>
+                <View>
                 {pending?.map(item => (
-                    <View>
+                    <View style={styles.inneritem}>
                         <Text style={styles.title}>{item}</Text>
                 </View>
                 ))}
@@ -164,14 +168,16 @@ const ViewFriends = () => {
 
       const displayRequests = () =>{
         if (requests.length == 0){
-            return <Text style={styles.details}>You currently do not have any requests.</Text>
+            return <Text style={styles.title}>You currently do not have any requests.</Text>
         }
         else{
             return(
-                <View style={styles.item}>
+                <View>
                 {requests?.map(item => (
-                    <View>
+                    <View style={styles.inneritem}>
                         <Text style={styles.title}>{item}</Text>
+                        <Button title="Accept" onPress={() =>acceptRequest(item)}></Button>
+                        <Button title="Deny"></Button>
                 </View>
                 ))}
                 </View>
@@ -179,25 +185,113 @@ const ViewFriends = () => {
         }
       }
 
+      const acceptRequest = async (name: String) =>{
+        try {
+            const { data: { user }, } = await supabase.auth.getUser();
+            setUserid(user?.id);
+        
+            const { error: updateError } = await supabase
+            .from('friends')
+            .update({
+              status:'friends'
+            })
+            .eq('employee1_name', name)
+            .eq('employee2_id', user?.id);
+    
+          if (updateError) {
+            console.error('Error updating friend data:', updateError.message);
+            return;
+          }
+        } 
+        catch{
+            console.log("Error logging friend data")
+    }
+    try {
+        const { data: { user }, } = await supabase.auth.getUser();
+        setUserid(user?.id);
+    
+        const { error: updateError } = await supabase
+        .from('friends')
+        .update({
+          status:'friends'
+        })
+        .eq('employee2_name', name)
+        .eq('employee1_id', user?.id);
+
+      if (updateError) {
+        console.error('Error updating friend data:', updateError.message);
+        return;
+      }
+    } 
+    catch{
+        console.log("Error logging friend data")
+}
+        router.navigate('./');
+      }
+
+      const denyRequest = async (name: String) =>{
+        try {
+            const { data: { user }, } = await supabase.auth.getUser();
+            setUserid(user?.id);
+        
+            const { error: updateError } = await supabase
+            .from('friends')
+            .delete()
+            .eq('employee1_name', name)
+            .eq('employee2_id', user?.id);
+    
+          if (updateError) {
+            console.error('Error updating friend data:', updateError.message);
+            return;
+          }
+        } 
+        catch{
+            console.log("Error logging friend data")
+    }
+    try {
+        const { data: { user }, } = await supabase.auth.getUser();
+        setUserid(user?.id);
+    
+        const { error: updateError } = await supabase
+        .from('friends')
+        .delete()
+        .eq('employee2_name', name)
+        .eq('employee1_id', user?.id);
+
+      if (updateError) {
+        console.error('Error updating friend data:', updateError.message);
+        return;
+      }
+    } 
+    catch{
+        console.log("Error logging friend data")
+}
+        router.navigate('./');
+      }
+
+
 
     return (
-        <LinearGradient colors={['#1a7373', '#e37b60']} style={{height:'100%', width:'100%'}}>
-        <View>
-        <Text style={styles.details}>Friends: </Text>
+        <View style={styles.container}>
+        <LinearGradient colors={['#eccbaa', '#65AAB3']} style={{height:'100%', width:'100%'}}>
+        <ScrollView >
+        <Button title="Add Friends" onPress={() => router.navigate('./Friends')}></Button>
+        <Text style={styles.header}>Friends: </Text>
         <View style={styles.item}>
             {displayFriends()}
             </View>
-        <Text style={styles.details}>Requests: </Text>
+        <Text style={styles.header}>Requests: </Text>
         <View style={styles.item}>
             {displayRequests()}
             </View>
-            <Text style={styles.details}>Pending: </Text>
+            <Text style={styles.header}>Pending: </Text>
         <View style={styles.item}>
             {displayPending()}
             </View>
-        </View>
+        </ScrollView>
         </LinearGradient>
-        
+        <NavBar/>
+        </View>
     );
 };
 
@@ -209,29 +303,53 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#fff',
+        height: '100%',
       },
+    item: {
+        margin: 10,
+        borderBottomWidth: 2,
+        borderBottomColor: "lightgrey",
+        backgroundColor:'white',
+        color:'black',
+        padding:10,
+        borderRadius:10,
+        elevation:10,
+        display: 'flex',
+        marginBottom: 50,
+      },
+
+      inneritem: {
+        display: 'flex',
+        flexDirection: 'row',
+      },
+
+      title: {
+        fontSize: 20,
+        fontWeight: "bold",
+        marginBottom: 5,
+        fontStyle: "italic",
+        color: 'black',
+      },
+    
+      details: {
+        fontSize: 15,
+        fontWeight: "bold",
+        marginBottom: 5,
+        fontStyle: "italic",
+        color: 'black',
+        borderColor:'gray',
+        borderBottomWidth:1,
+      },
+      header: {
+        fontSize: 35,
+        fontWeight: "bold",
+        marginBottom: 35,
+        color: 'white',
+        alignSelf:'center',
+        marginTop: 35,
+      },
+    
   
-      item: {
-          margin: 30,
-          borderBottomWidth: 2,
-          borderBottomColor: "lightgrey",
-          
-        },
-        title: {
-          fontSize: 20,
-          fontWeight: "bold",
-          marginBottom: 5,
-          fontStyle: "italic",
-          color: 'white',
-        },
-      
-        details: {
-          fontSize: 15,
-          fontWeight: "bold",
-          marginBottom: 5,
-          fontStyle: "italic",
-          color: 'white',
-        },
   });
 
   export default ViewFriends;
